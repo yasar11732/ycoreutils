97b134e7babc9a7544b6165258edd2449b816d2e
Yasar Arabaci <yasar11732@gmail.com>
2012-04-12 08:42:44 +0300

    Make yes accept multiple arguments

--- src/yes.c
+++ src/yes.c
@@ -2,13 +2,17 @@
 
 int main(int argc,char *argv[])
 {
-    char *str;
-    if (argc == 2)
-        str = argv[1];
-    else
-        str = "y";
+    if (argc > 1)
+        while (1) {
+            int i;
+            
+            for(i = 1; i < argc - 1; ++i)
+                printf("%s ",argv[i]);
 
-    while (1)
-        printf("%s\n",str);
+            printf("%s\n",argv[argc-1]);
+        }
+    else
+        while (1)
+            printf("y\n");
     return 0;
 }
