2373965145aa32a24cfa7e280072e721e18d11d1
Yasar Arabaci <yasar11732@gmail.com>
2012-04-12 06:49:34 +0300

    Improve lib/utilities.c
    Add some comments, and correct strtoken to return
    new string if delimeter cannot be found.

--- lib/utilities.c
+++ lib/utilities.c
@@ -2,6 +2,12 @@
 #include <string.h> // memcpy
 #include "utilities.h"
 
+/*
+ * Kind of a atoi clone. Making my own functions makes
+ * me happier, since I know for sure how my functions
+ * are implemented. It makes me confident!
+ */
+
 int
 str2int(const char * const str)
 {
@@ -32,14 +38,27 @@ strfind(char * const string,const char * const chr)
     return NULL;
 }
 
+/*
+ * My version of strtok with some differences, because everyone knows
+ * that strtok sucks;
+ * If no delimeteter found in input, return a copy of input (this is
+ * important, because caller will assume new string is returned), and
+ * set *rest to end of input. Otherwise, return new string
+ * up until the delimeter and set *rest to first position after the 
+ * delimeter in input.
+ */
+
 char *
 strtoken(char * const input, const char * const delim,char **rest) {
     char *delimpos = strfind(input, delim);
 
     if (delimpos == NULL) {
         // no delimeter in input
-        *rest = NULL;
-        return input;
+        size_t inputlength = strlen(input);
+        char *result = malloc(sizeof(char) * (inputlength + 1));
+        strcpy(result,input);
+        *rest = &(input[inputlength]);
+        return result;
     }
     int length = delimpos - input;
     char *result = malloc(sizeof(char) * (length + 1));
