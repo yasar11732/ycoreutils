9692602a59e54e3d4d1080e4d52ebfc64d5e5795
Yasar Arabaci <yasar11732@gmail.com>
2012-04-19 06:49:09 +0300

    Improve readability in mkdir

--- src/mkdir.c
+++ src/mkdir.c
@@ -7,52 +7,55 @@
 #include "gstring.h" // gstring thingies.
 #include "utilities.h"
 
+void exit_with_error(const char const *programname, const char const *componentname) {
+	fprintf(stderr, "%s: %s: %s\n", programname, strerror(errno), componentname);
+	exit(errno);
+}
+
 int main(int argc, char *argv[])
 {
 	mode_t mask = umask(0);
 	mode_t mode = 0777&~mask;
 
-	int verbose = 0;
-	int parent  = 0;
-	int c;
+	int verboseflag = 0;
+	int parentflag = 0;
+	int option;
 	int i;
 
-	while ((c = getopt(argc, argv, "pvm:")) != -1)
-		switch (c) {
+	while ((option = getopt(argc, argv, "pvm:")) != -1)
+		switch (option) {
 		case 'm':
 			mode = strtol(optarg,NULL,8);
 			break;
 		case 'p':
-			parent = 1;
+			parentflag = 1;
 			break;
 		case 'v':
-			verbose = 1;
+			verboseflag = 1;
 			break;
 		case '?':
 			exit(EXIT_FAILURE);
 		default:
-			abort ();
+			abort (); // this shouldn't happen!
 		}
+	
 	// Handle other command line arguments
 	for (i = optind; i < argc; ++i) {
 		errno = 0;
-		if (parent) {
+		if (parentflag) {
+			/* Important: Don't delete *gargv until while loop
+			 * finishes, rest's content is just pointer to it's
+			 * content. */
 			struct Gstring rest;
 			struct Gstring *gargv = togstring(argv[i]);
 			struct Gstring *path_so_far = gstrtoken(gargv,"/",&rest);
 
 			while(rest.length > 0) {
 				struct Gstring *next_part;
-				if (path_so_far->length > 0 &&
-						file_exists(path_so_far->content) == -1) {
-					if (mkdir(path_so_far->content, mode) == -1) {
-						fprintf(stderr, "%s: %s: %s",
-								argv[0],
-								strerror(errno),
-								argv[i]);
-						exit(EXIT_FAILURE);
-					}
-				}
+
+				if(mkdir(path_so_far->content, mode) == -1 && errno != EEXIST)
+					exit_with_error(argv[0], argv[i]);
+
 				next_part = gstrtoken(&rest,"/",&rest);
 				appendstr2gstring(path_so_far,"/");
 				combinegstr(path_so_far,next_part);
@@ -61,14 +64,11 @@ int main(int argc, char *argv[])
 			freegstring (path_so_far);
 		}
 
-		if (mkdir(argv[i],mode) == -1) {
-			if (!parent || errno != EEXIST) {
-				fprintf(stderr, "%s: %s: %s\n",
-						argv[0],strerror(errno), argv[i]);
-				exit(EXIT_FAILURE);
-			}
-		}
-		if (verbose && errno != EEXIST)
+		if (mkdir(argv[i],mode) == -1)
+			if (!parentflag || errno != EEXIST)
+				exit_with_error(argv[0], argv[i]);
+
+		if (verboseflag && errno != EEXIST)
 			printf("%s created.\n",argv[i]);
 	}
 	return 0;
