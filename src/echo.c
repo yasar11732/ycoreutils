1dcc788911ee94250621458bfb4bcc67f0c03692
Yasar Arabaci <yasar11732@gmail.com>
2012-04-19 08:29:16 +0300

    Add echo.c

---
+++ src/echo.c
@@ -0,0 +1,42 @@
+#include <stdlib.h>
+#include <unistd.h> // getopt
+#include <stdio.h>
+#include <string.h>
+#include "utilities.h"
+int main(int argc, char *argv[])
+{
+	int nonewlineflag = 0;
+	int escapeflag = 0;
+	int option;
+	int i;
+
+	while ((option = getopt(argc, argv, "neE")) != -1)
+		switch (option) {
+		case 'n':
+			nonewlineflag = 1;
+			break;
+		case 'e':
+			escapeflag = 1;
+			break;
+		case 'E':
+			escapeflag = 0;
+			break;
+		case '?':
+			exit(EXIT_FAILURE);
+		default:
+			abort (); // this shouldn't happen!
+		}
+	
+	for (i = optind; i < argc; ++i) {
+		/* Don't put space before first argument! */
+		if (i != optind)
+			fwrite(" ",1,1,stdout);
+		if (escapeflag)
+			translate_escapes(argv[i]);
+		printf(argv[i]);
+	}
+
+	if (!nonewlineflag)
+		fwrite("\n",1,1,stdout);
+	return 0;
+}
